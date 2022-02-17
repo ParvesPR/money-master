15acda9da32dfe45ea9505e21f79d5325ac56744
ParvesPR <parves2585@gmail.com>
2022-02-17 14:42:30 +0800

    function added

--- js/money.js
+++ js/money.js
@@ -1,1 +1,21 @@
-alert('hello')
+// Define input value
+function getInput(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputValue = parseFloat(inputField.value);
+    return inputValue;
+}
+
+
+document.getElementById('calc-btn').addEventListener('click', function () {
+    const foodInput = getInput('food-input');
+    const rentInput = getInput('rent-input');
+    const clothesInput = getInput('clothes-input');
+    const totalInputAmount = foodInput + rentInput + clothesInput;
+
+    // total balance
+    const expensesBalance = document.getElementById('expenses-amount');
+    const expensesAmount = parseFloat(expensesBalance.innerText);
+    const totalExpenses = expensesAmount + totalInputAmount;
+    expensesBalance.innerText = totalExpenses;
+
+})
